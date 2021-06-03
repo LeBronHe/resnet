75332af6fa60e3023a0aee269960b46dda3ab9e7
Anna Nordstrand <anna@nordstrand.dev>
2021-06-02 19:07:31 -0500

    refactor: random cleanup

--- download_mnist.py
+++ download_mnist.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from urllib.parse import urljoin
-from urllib.request import urlretrieve
-from urllib.error import URLError
 import gzip
 import shutil
 
-PATH = Path("data/mnist")
+import urllib.request
+from urllib.error import URLError
+import urllib.parse
+
+MNIST_PATH = Path("data/mnist")
+
 BASE_URL: str = "https://storage.googleapis.com/cvdf-datasets/mnist/"
 
 RESOURCES: list[str] = [
@@ -19,11 +21,12 @@ def download(url: str, destination_path: Path):
     if destination_path.exists():
         return
     else:
-        print(f"Downloading {destination_path}...")
+        file_name: str = destination_path.name
+        print(f"Downloading {file_name}...")
         try:
-            urlretrieve(url, destination_path)
+            urllib.request.urlretrieve(url, destination_path)
         except URLError:
-            raise URLError("Failed to download resource")
+            raise URLError(f"Failed to download {file_name}")
 
 def unzip(file_path: Path):
     unzipped_path = file_path.with_suffix('')
@@ -32,19 +35,22 @@ def unzip(file_path: Path):
 
     with gzip.open(file_path, "rb") as zipped_file:
         with open(unzipped_path, "wb") as unzipped_file:
-            print(f"Unzipping {file_path}...")
             shutil.copyfileobj(zipped_file, unzipped_file)
+            print(f"Unzipped {file_path.name}")
 
 def main():
-    if not PATH.exists():
-        PATH.mkdir(parents=True, exist_ok=True)
+    if not MNIST_PATH.exists():
+        MNIST_PATH.mkdir(parents=True, exist_ok=True)
 
-    for resource in RESOURCES:
-        url: str = urljoin(BASE_URL, resource)
-        path = PATH.joinpath(resource)
+    try:
+        for resource in RESOURCES:
+            url: str = urllib.parse.urljoin(BASE_URL, resource)
+            path = MNIST_PATH.joinpath(resource)
 
-        download(url, path)
-        unzip(path)
+            download(url, path)
+            unzip(path)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
     main()
