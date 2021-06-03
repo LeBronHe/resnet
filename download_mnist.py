1c869d5ff066df44216780c08bae052f834aebdc
Anna Nordstrand <anna@nordstrand.dev>
2021-06-03 09:58:05 -0500

    feat!: setup CMake

--- download_mnist.py
+++ download_mnist.py
@@ -1,10 +1,9 @@
-from pathlib import Path
 import gzip
 import shutil
-
+from pathlib import Path
 import urllib.request
-from urllib.error import URLError
 import urllib.parse
+from urllib.error import URLError
 
 MNIST_PATH = Path("data/mnist")
 
@@ -19,24 +18,23 @@ RESOURCES: list[str] = [
 
 def download(url: str, destination_path: Path):
     if destination_path.exists():
+        print(f"{destination_path} already exists, skipping...")
         return
     else:
-        file_name: str = destination_path.name
-        print(f"Downloading {file_name}...")
         try:
             urllib.request.urlretrieve(url, destination_path)
         except URLError:
-            raise URLError(f"Failed to download {file_name}")
+            raise URLError(f"Failed to download {destination_path.name}")
 
 def unzip(file_path: Path):
     unzipped_path = file_path.with_suffix('')
     if (unzipped_path.exists()):
+        print(f"{unzipped_path} already exists, skipping...")
         return
 
     with gzip.open(file_path, "rb") as zipped_file:
         with open(unzipped_path, "wb") as unzipped_file:
             shutil.copyfileobj(zipped_file, unzipped_file)
-            print(f"Unzipped {file_path.name}")
 
 def main():
     if not MNIST_PATH.exists():
@@ -50,7 +48,7 @@ def main():
             download(url, path)
             unzip(path)
     except KeyboardInterrupt:
-        pass
+        print("Unable to continue, received KeyboardInterrupt")
 
 if __name__ == "__main__":
     main()
---
+++ src/main.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string_view>
+#include <vector>
+
+auto main(int argc, char** argv) -> int {
+    std::vector<std::string_view> args(argv + 1, argv + argc);
+
+    for (const auto& arg : args) {
+        std::cout << arg << std::endl;
+    }
+
+    return 0;
+}
