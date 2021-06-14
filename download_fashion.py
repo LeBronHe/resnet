93afa1decc6ff64f692023b6f838278b758e3133
Anna Nordstrand <anna@nordstrand.dev>
2021-06-14 08:49:15 -0500

    fix: extract downloaded files in memory

--- download_fashion.py
+++ download_fashion.py
@@ -1,16 +1,19 @@
 import gzip
-import os
-from pathlib import Path
 import concurrent.futures
 import urllib.request
+import urllib.parse
+from io import BytesIO
+from pathlib import Path
 
 FASHION_PATH = Path("./data/fashion")
 
-URLS = [
-    "https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/train-images-idx3-ubyte.gz",
-    "https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/train-labels-idx1-ubyte.gz",
-    "https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/t10k-images-idx3-ubyte.gz",
-    "https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/t10k-labels-idx1-ubyte.gz"
+BASE_URL = "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+
+RESOURCES = [
+    "train-images-idx3-ubyte.gz",
+    "train-labels-idx1-ubyte.gz",
+    "t10k-images-idx3-ubyte.gz",
+    "t10k-labels-idx1-ubyte.gz"
 ]
 
 def download(url, destination):
@@ -18,17 +21,21 @@ def download(url, destination):
         print(f"{destination} already exists, skipping...")
         return
 
-    with urllib.request.urlopen(url) as response:
-        with open(destination, "wb") as output:
-            output.write(gzip.decompress(response.read()))
+    response = urllib.request.urlopen(url)
+
+    with BytesIO(response.read()) as compressed:
+        with gzip.GzipFile(fileobj=compressed) as uncompressed:
+            with open(destination, "wb") as output:
+                output.write(uncompressed.read())
 
 def main():
     if not FASHION_PATH.is_dir():
         FASHION_PATH.mkdir(parents=True, exist_ok=True)
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        for url in URLS:
-            destination = FASHION_PATH.joinpath(os.path.basename(url)).with_suffix("")
+        for resource in RESOURCES:
+            url = urllib.parse.urljoin(BASE_URL, resource)
+            destination = FASHION_PATH.joinpath(resource).with_suffix("")
             executor.submit(download, url, destination)
 
 if __name__ == "__main__":
