7802a4719b8d56c462d0b8d752ad6f2e4d52f8ae
Anna Nordstrand <anna@nordstrand.dev>
2021-07-22 21:57:26 -0500

    feat: performance improvements, add shuffle for train data

--- src/mnist.cxx
+++ src/mnist.cxx
@@ -2,18 +2,19 @@
 
 #include <algorithm>
 #include <fstream>
+#include <random>
 #include <stdexcept>
 
 Mnist::Mnist(const fs::path& root) : root{root} {}
 
-auto Mnist::load_train_data() -> std::pair<std::vector<float>*, std::vector<float>*> {
+auto Mnist::load_train_data() -> std::pair<Vector2d*, Vector2d*> {
     read_file(train_images, TRAIN_IMAGES_FILENAME);
     read_file(train_labels, TRAIN_LABELS_FILENAME);
 
     return std::make_pair(&train_images, &train_labels);
 }
 
-auto Mnist::load_test_data() -> std::pair<std::vector<float>*, std::vector<float>*> {
+auto Mnist::load_test_data() -> std::pair<Vector2d*, Vector2d*> {
     read_file(test_images, TEST_IMAGES_FILENAME);
     read_file(test_labels, TEST_LABELS_FILENAME);
 
@@ -27,7 +28,7 @@ auto Mnist::read_header(std::unique_ptr<char[]>& buf, std::size_t idx) const -> 
     return (x >> 24) | ((x & 0x00FF0000) >> 8) | ((x & 0x0000FF00) << 8) | (x << 24);
 }
 
-auto Mnist::read_file(std::vector<float>& vec, const fs::path& filename) -> void {
+auto Mnist::read_file(Vector2d& vec, const fs::path& filename) -> void {
     fs::path path = root / filename;
     std::ifstream file(path.c_str(), std::ios::binary);
     if (!file) {
@@ -44,25 +45,32 @@ auto Mnist::read_file(std::vector<float>& vec, const fs::path& filename) -> void
     auto magic_number = read_header(buf, 0);
     auto count = read_header(buf, 1);
 
+    vec.reserve(count);
+    
     if (magic_number == IMAGE_MAGIC_NUMBER) {
         auto height = read_header(buf, 2);
         auto width = read_header(buf, 3);
-        std::size_t pixels = count * height * width;
-
-        vec.reserve(pixels);
 
         auto ptr = reinterpret_cast<std::uint8_t*>(buf.get() + 16);
-        for (std::size_t idx = 0; idx < pixels; ++idx) {
-            auto pixel = *ptr++;
-            vec.push_back(pixel / 255.0f);
+        for (std::size_t i = 0; i < count; ++i) {
+            auto image = std::make_unique<float[]>(height * width);
+
+            for (std::size_t j = 0; j < height * width; ++j) {
+                auto pixel = *ptr++;
+                image[j] = pixel / 255.0f;
+            }
+
+            vec.push_back(std::move(image));
         }
     } else if (magic_number == LABEL_MAGIC_NUMBER) {
-        vec.assign(count * CLASSES, 0.0f);
-
         auto ptr = reinterpret_cast<std::uint8_t*>(buf.get() + 8);
-        for (std::size_t idx = 0; idx < count; ++idx) {
+        for (std::size_t i = 0; i < count; ++i) {
+            auto one_hot_vec = std::make_unique<float[]>(CLASSES);
             auto label = *ptr++;
-            vec[(idx * CLASSES) + label] = 1.0f;
+            
+            one_hot_vec[label] = 1.0f;
+
+            vec.push_back(std::move(one_hot_vec));
         }
     } else {
         throw std::runtime_error("Invalid MNIST file");
@@ -70,4 +78,10 @@ auto Mnist::read_file(std::vector<float>& vec, const fs::path& filename) -> void
 }
 
 auto Mnist::shuffle() -> void {
+    std::random_device rd;
+    std::mt19937 g_images(rd());
+    auto g_labels = g_images;
+
+    std::shuffle(std::begin(train_images), std::end(train_images), g_images);
+    std::shuffle(std::begin(train_labels), std::end(train_labels), g_labels);
 }
--- src/mnist.hxx
+++ src/mnist.hxx
@@ -11,40 +11,43 @@ namespace fs = std::filesystem;
 
 class Mnist {
 public:
+    using Vector2d = std::vector<std::unique_ptr<float[]>>;
+
     Mnist(const fs::path& root);
     Mnist(const Mnist&) = delete;
 
     ~Mnist() = default;
 
     [[nodiscard]]
-    auto load_train_data() -> std::pair<std::vector<float>*, std::vector<float>*>;
+    auto load_train_data() -> std::pair<Vector2d*, Vector2d*>;
     
     [[nodiscard]]
-    auto load_test_data() -> std::pair<std::vector<float>*, std::vector<float>*>;
+    auto load_test_data() -> std::pair<Vector2d*, Vector2d*>;
 
     auto shuffle() -> void;
 
     auto operator=(const Mnist&) = delete;
 
-//private:
+private:
     static constexpr std::uint32_t IMAGE_MAGIC_NUMBER = 0x0000'0803;
     static constexpr std::uint32_t LABEL_MAGIC_NUMBER = 0x0000'0801;
     static constexpr std::size_t CLASSES = 10;
 
-    fs::path root;
     static constexpr const char* TRAIN_IMAGES_FILENAME = "train-images-idx3-ubyte";
     static constexpr const char* TRAIN_LABELS_FILENAME = "train-labels-idx1-ubyte";
     static constexpr const char* TEST_IMAGES_FILENAME = "t10k-images-idx3-ubyte";
     static constexpr const char* TEST_LABELS_FILENAME = "t10k-labels-idx1-ubyte";
-
-    std::vector<float> train_images;
-    std::vector<float> train_labels;
-    std::vector<float> test_images;
-    std::vector<float> test_labels;
+    fs::path root;
+    
+public:
+    Vector2d train_images;
+    Vector2d train_labels;
+    Vector2d test_images;
+    Vector2d test_labels;
 
     auto read_header(std::unique_ptr<char[]>& buf, std::size_t idx) const -> std::uint32_t;
 
-    auto read_file(std::vector<float>& vec, const fs::path& filename) -> void;
+    auto read_file(Vector2d& vec, const fs::path& filename) -> void;
 };
 
 #endif /* MNIST_HXX */
